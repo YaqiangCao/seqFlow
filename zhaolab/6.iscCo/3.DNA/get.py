c1975f7bf8c80289d7e9e5eed45f98ce013f3222
YaqiangCao <caoyaqiang0410@gmail.com>
2022-07-25 11:46:36 -0400

    iscCo

---
+++ zhaolab/6.iscCo/3.DNA/get.py
@@ -0,0 +1,12 @@
+import os
+from glob import glob
+
+key = "DNA"
+fs = glob("../2.sepSingleDNARNA/*%s_R1.fastq.gz"%key)
+fs.sort()
+cmd = "cat %s > %s_R1.fastq.gz"%(" ".join(fs),key)
+print(cmd)
+os.system(cmd)
+cmd = cmd.replace("_R1.fastq.gz","_R2.fastq.gz")
+print(cmd)
+os.system(cmd)
