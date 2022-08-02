abe8d79a65bf704bc205c7d7c2aa8131e201aca0
YaqiangCao <caoyaqiang0410@gmail.com>
2022-08-02 15:37:48 -0400

    KZ2471

--- zhaolab/1.PE_CHIC/run.sh
+++ zhaolab/1.PE_CHIC/run.sh
@@ -12,9 +12,9 @@ cd $d/4.reduBedpe/
 python getReduBedpe.py
 
 cd $d/5.bdgBws/ 
-#python bedpe2bg.py -pattern '../4.reduBedpe/*.bedpe.gz' -mapq 10 -cpu 10
-#python bdg2bw.py -pattern '*.bdg' -org mm10 -cpu 10
-python getBw.py
+python bedpe2bg.py -pattern '../4.reduBedpe/*.bedpe.gz' -mapq 10 -cpu 10
+python bdg2bw.py -pattern '*.bdg' -org hg38 -cpu 10
+#python getBw.py
 
 #conda activate cLoops2
 #cd $d/6.cLoops2/
