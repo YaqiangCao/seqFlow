bf401d0e33caa8eb282f32d97d11da152ff9227c
YaqiangCao <caoyaqiang0410@gmail.com>
2023-01-24 16:30:41 -0500

    updates

--- zhaolab/7.MNase/6.ncwos/get.py
+++ zhaolab/7.MNase/6.ncwos/get.py
@@ -3,6 +3,6 @@ from glob import glob
 fs = glob("../5.tsv/*.tsv.gz")
 for f in fs:
     n = f.split("/")[-1].split(".tsv.gz")[0]
-    cmd = f"getNucCWOS.py -f {f} -o {n} -cof ~/caoy7/Projects/0.Reference/2.mm10/1.fa/mm10.chrom.sizes -bw &"
+    cmd = f"python3 getNucCWOS.py -f {f} -o {n} -csf ~/caoy7/Projects/0.Reference/2.mm10/1.fa/mm10.chrom.sizes -bw &"
     print(cmd)
     os.system(cmd)
--- zhaolab/7.MNase/run2.sh
+++ zhaolab/7.MNase/run2.sh
@@ -1,6 +1,9 @@
 d=`pwd`
 echo $d
 
+cd $d/4.reduBedpe/ 
+python bedpeStat.py &
+
 cd $d/6.ncwos/
 python get.py 
 
